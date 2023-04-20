fcfd20d2e75afdc76cf5a3ceeb959f445a29c873
modrinicanja <am20190184@student.fon.bg.ac.rs>
2023-04-20 18:44:20 +0200

    azuriran app.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,110 @@ import './App.css';
 import "./App.css";
 import skije from "./komponente/skije";
 import NavMeni from "./komponente/NavMeni.jsx";
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Cart from "./komponente/Cart";
+import Home from "./komponente/Home";
+import Contact from "./komponente/Contact";
 
 function App() {
+  const [cartNum, setCartNum] =
+    useState(0);
+  const [
+    cartProducts,
+    setCartProducts,
+  ] = useState([]);
+  const [products] = useState([
+    {
+      id: 1,
+      title:
+        "Skije 1",
+      img: "https://img.gigatron.rs/img/products/large/image6135e0e9b547f.png",
+      price: "45000 RSD",
+      description:
+        "    Salomon E S/Force Fx.80 skije i vez",
+        amount: 0,
+    },
+    {
+      id: 2,
+      title:
+        "Skije 2",
+      img: "https://img.gigatron.rs/img/products/large/image6135e0e9b547f.png",
+      price: "45000 RSD",
+      description:
+        "    Salomon E S/Force Fx.80 skije i vez",
+        amount: 0,
+    },
+    {
+      id: 3,
+      title:
+        "Skije 3",
+      img: "https://img.gigatron.rs/img/products/large/image6135e0e9b547f.png",
+      price: "45000 RSD",
+      description:
+        "    Salomon E S/Force Fx.80 skije i vez",
+        amount: 0,
+    },
+    {
+      id: 4,
+      title:
+        "Skije 4",
+      img: "https://img.gigatron.rs/img/products/large/image6135e0e9b547f.png",
+      price: "45000 RSD",
+      description:
+        "    Salomon E S/Force Fx.80 skije i vez",
+        amount: 0,
+    },
+  ]);
+  function refresh() {
+    let newProducts = products.filter(
+      (prod) => prod.amount > 0
+    );
+    setCartProducts(newProducts);
+  }
+  function addProduct(id) {
+    setCartNum(cartNum + 1);
+    products.forEach((prod) => {
+      if (prod.id === id) {
+        prod.amount++;
+      }
+    });
+    refresh();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <NavMeni />
-      <skije />
-    </div>
+    <BrowserRouter className="App">
+      <NavMeni cartNum={cartNum} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+        <Route
+          path="/laptops"
+          element={
+            <Laptops
+              products={products}
+              onAdd={addProduct}
+            />
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Cart
+              products={cartProducts}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
